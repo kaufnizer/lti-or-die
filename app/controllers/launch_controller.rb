da14753e1719c70ce903fc686cbf3659365db712
Jesse Poulos <poulos.j.m@gmail.com>
2016-06-06 22:10:16 -0600

    disable x-frame-options

--- app/controllers/launch_controller.rb
+++ app/controllers/launch_controller.rb
@@ -1,10 +1,12 @@
 class LaunchController < ActionController::Base
   def show
     #@launch_params ||= nil
+    response.headers.delete "X-Frame-Options"
     render text: "These were the last launch params: #{$launch_params}"
   end
 
   def receive
+    response.headers.delete "X-Frame-Options"
     $launch_params = request.body.read
     render text: "Thanks for sending a POST request with cURL! Payload: #{$launch_params}"
   end
