7c6a1b826e778f2908e0765eaea410a3a11c4527
Jesse Poulos <poulos.j.m@gmail.com>
2016-07-06 10:54:33 -0600

    switch to views, rick roll on invalid secret, other stuff

--- app/controllers/launch_controller.rb
+++ app/controllers/launch_controller.rb
@@ -11,11 +11,11 @@ class LaunchController < ActionController::Base
     @domain = @launch_params["custom_canvas_api_domain"]
   end
 
-  def show
-    #@launch_params ||= nil
-    response.headers.delete "X-Frame-Options"
-    render text: "These were the last launch params: #{@launch_params}"
-  end
+  #def show
+  #  #@launch_params ||= nil
+  #  response.headers.delete "X-Frame-Options"
+  #  render text: "These were the last launch params: #{@launch_params}"
+  #end
 
   def receive
     response.headers.delete "X-Frame-Options"
@@ -23,8 +23,9 @@ class LaunchController < ActionController::Base
     #lti_message = IMS::LTI::Models::Messages::Message.generate(request.request_parameters.merge(request.query_parameters))
     #lti_message.launch_url = request.url
     secret = '1'
+    puts request.parameters
 
-    provider_signature = OAuth::Signature.sign(request, :consumer_secret => secret)
+    provider_signature = OAuth::Signature.sign(request, :consumer_secret => secret, :consumer_key => '1')
 
     if provider_signature == @launch_params["oauth_signature"]
     #if lti_message.valid_signature?(secret)
@@ -40,17 +41,19 @@ class LaunchController < ActionController::Base
         request_access
       else
         puts "User already has token"
-        render text: "Signature is valid, here are your launch params\n\n Payload:\n\n #{@launch_params}"
+        render :show
       end
     else
-      render text: "Invalid signature!\n\n Payload:\n\n #{@launch_params}"
+      render :invalid_signature
     end
   end
 
     def request_access
-      url = "https://#{@domain}/login/oauth2/auth?client_id=36990000000000001&response_type=code&redirect_uri=http://localhost:3000/oauth2response&state=#{@user_id}"
+      url = "https://#{@domain}/login/oauth2/auth?client_id=37000000000000001&response_type=code&redirect_uri=http://localhost:3000/oauth2response&state=#{@user_id}"
       redirect_to url
     end
+#https://jpoulos.instructure.com/login/oauth2/auth?client_id=37000000000000002&response_type=code&redirect_uri=http://google.com&scope=/auth/userinfo
+#https://jpoulos.instructure.com/login/oauth2/auth?client_id=37000000000000002&response_type=code&redirect_uri=http://google.com&scope=
 
   def oauth2response
 
@@ -59,6 +62,7 @@ class LaunchController < ActionController::Base
     state = request.params["state"]
     puts "state= #{state}"
     user = User.find_by(user_id: state)
+    puts user
     domain = user["token_requested_from"]
 
     if user["canvas_api_refresh_token"] && Time.now.to_i > user["token_expires_at"]
@@ -66,8 +70,8 @@ class LaunchController < ActionController::Base
       request = Typhoeus::Request.new("https://#{domain}/login/oauth2/token",
                                       method: :post,
                                       params: {:grant_type=>"refresh_token",
-                                               :client_id=>"36990000000000001",
-                                               :client_secret => "i6FzleeV5D3IVv9ClYEJtfQ76NNGg7oGy59IUFMQB7JAWBhZSSDyJlnTPKiydO9g",
+                                               :client_id=>"37000000000000001",
+                                               :client_secret => "SAwDtcgVk9PsONsmVNEnBgRB339KheyfDdqLi9G1oIY1fbgcTFAoYD4NNstYPvHl",
                                                :redirect_uri => "https://localhost:3000/oauth2response",
                                                :refresh_token => user["canvas_api_refresh_token"]
                                       })
@@ -76,8 +80,8 @@ class LaunchController < ActionController::Base
       request = Typhoeus::Request.new("https://#{domain}/login/oauth2/token",
                                       method: :post,
                                       params: {:grant_type=>"authorization_code",
-                                               :client_id=>"36990000000000001",
-                                               :client_secret => "i6FzleeV5D3IVv9ClYEJtfQ76NNGg7oGy59IUFMQB7JAWBhZSSDyJlnTPKiydO9g",
+                                               :client_id=>"37000000000000001",
+                                               :client_secret => "SAwDtcgVk9PsONsmVNEnBgRB339KheyfDdqLi9G1oIY1fbgcTFAoYD4NNstYPvHl",
                                                :redirect_uri => "https://localhost:3000/oauth2response",
                                                :code => code
                                       })
@@ -101,6 +105,7 @@ class LaunchController < ActionController::Base
     response = request.run
     response_body = JSON.load response.response_body
 
-    redirect_to response_body["avatar_url"]
+    #redirect_to response_body["avatar_url"]
+    render :show
   end
 end
