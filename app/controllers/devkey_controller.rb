b2149c02c1ae04752d1d0e3e39e0af1b35db0377
Jesse Poulos <poulos.j.m@gmail.com>
2016-07-07 12:20:59 -0600

    added developer keys model

--- app/controllers/devkey_controller.rb
+++ app/controllers/devkey_controller.rb
@@ -1,2 +1,4 @@
 class DevkeyController < ApplicationController
+  def new
+  end
 end
---
+++ test/models/devkey_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class DevkeyTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
