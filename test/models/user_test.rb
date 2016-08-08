c13fce0c700ffadddc2d934e39fc4dde31acb3c2
Jesse Poulos <poulos.j.m@gmail.com>
2016-08-08 15:59:18 -0600

    set up tests and user fixtures; tests added for user model has_api_token? and token_valid methods

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "it detects whether a user has an API token" do
+    assert users(:has_valid_token).has_api_token?
+    assert_not users(:no_token).has_api_token?
+  end
+
+  test "it detects whether a user has a valid API token" do
+    assert users(:has_valid_token).token_valid?("https://jpoulos.instructure.com")
+    assert_not users(:no_token).token_valid?("https://jpoulos.instructure.com")
+  end
 end
