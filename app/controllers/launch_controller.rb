cbc3785e4cf17277f1ee84630755997bef0ba807
Jesse Poulos <poulos.j.m@gmail.com>
2016-08-08 13:48:28 -0600

    cleanup old comments

--- app/controllers/launch_controller.rb
+++ app/controllers/launch_controller.rb
@@ -10,27 +10,14 @@ class LaunchController < ActionController::Base
     @domain = @launch_params["custom_canvas_api_domain"]
   end
 
-  #def show
-  #  #@launch_params ||= nil
-  #  response.headers.delete "X-Frame-Options"
-  #  render text: "These were the last launch params: #{@launch_params}"
-  #end
-
   def receive
     response.headers.delete "X-Frame-Options"
-
-    #lti_message = IMS::LTI::Models::Messages::Message.generate(request.request_parameters.merge(request.query_parameters))
-    #lti_message.launch_url = request.url
     secret = '1'
-    #puts request.parameters
-
-
 
     @devkey = Devkey.find_by(domain: @domain)
     signature = ::OAuth2::Signature.new(request,secret)
 
     if signature.signature_valid? && @devkey
-    #if lti_message.valid_signature?(secret)
       @user = User.find_or_create_by(user_id: @user_id) do |user|
         user.full_name = @launch_params["lis_person_name_full"]
         user.primary_email = @launch_params["lis_person_contact_email_primary"]
@@ -57,8 +44,6 @@ class LaunchController < ActionController::Base
       url = "https://#{@domain}/login/oauth2/auth?client_id=#{@devkey.client_id}&response_type=code&redirect_uri=#{@devkey.uri}&state=#{@user_id}"
       redirect_to url
     end
-#https://jpoulos.instructure.com/login/oauth2/auth?client_id=37000000000000002&response_type=code&redirect_uri=http://google.com&scope=/auth/userinfo
-#https://jpoulos.instructure.com/login/oauth2/auth?client_id=37000000000000002&response_type=code&redirect_uri=http://google.com&scope=
 
   def oauth2response
 
@@ -105,13 +90,6 @@ class LaunchController < ActionController::Base
     puts "Token updated to: #{response_body["access_token"]}"
     puts user.canvas_api_token
 
-    #request = Typhoeus::Request.new("http://#{domain}/api/v1/users/self",
-    #                                headers: {:Authorization=>"Bearer #{user["canvas_api_token"]}"
-    #                                })
-    #response = request.run
-    #response_body = JSON.load response.response_body
-
-    #redirect_to response_body["avatar_url"]
     render :show
   end
 end
